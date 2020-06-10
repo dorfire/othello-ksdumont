8ebc099b3c71724e659c54a8b3a8c5729671ec99
Keith Dumont <keithdumont@Keiths-MacBook-Pro.local>
2020-06-10 09:15:44 -0700

    initial commit --setup component structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Othello from "./Components/Othello/Othello";
+import Completed from "./Components/Completed/Completed";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameStatus: "active",
+      winner: null,
+      blackScore: 0,
+      whiteScore: 0,
+    };
+  }
+  render() {
+    let othello = this.state.gameStatus === "active" ? <Othello /> : "";
+    let completed = this.state.status === "completed" ? <Completed /> : "";
+    return (
+      <div className="App container">
+        <h1 className="text-center">Othello</h1>
+        {othello}
+        {completed}
+      </div>
+    );
+  }
 }
 
 export default App;
